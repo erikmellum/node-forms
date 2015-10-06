57894271b0a0e29940e0383b58119c8a8311daf5
Erik Mellum <emellum@mail.csuchico.edu>
2015-10-06 00:54:00 -0700

    first yep

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 var formdata = require('form-data')
 var fs       = require('fs')
+var http     = require('http')
 
 var fd = new formdata();
-fd.append('attachment', fs.createReadStream('/Users/emellum/Desktop/projects/launchpad_projects/tester/../tester.zip'))
+fd.append('attachment', fs.createReadStream('/Users/emellum/Desktop/projects/launchpad_projects/apps.zip'))
 fd.append('name', 'test');
 fd.append('description', 'test');
 fd.append('token', 'b3b2a169f471742feb38e263c5043812fc71b654');
 fd.append('project_id', 2);
+var headers = fd.getHeaders();
+headers.Authentication = 'b3b2a169f471742feb38e263c5043812fc71b654'
+var request = http.request({
+  method: 'post',
+  host: 'host.zurb.com',
+  path: '/api/v1/prototypes',
+  headers: headers
+});
+
+fd.pipe(request);
+
+request.on('response', function(res) {
+  console.log(res.statusCode);
+  console.log(res);
+  console.log(res.hosted_url);
+});
 fd.submit('https://host.zurb.com/api/v1/prototypes', function(err, data){
   console.log(err);
-  console.log(data);
+  console.log(data.hosted_url);
 })
