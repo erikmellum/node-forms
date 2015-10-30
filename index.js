187b4c24ded75c49091784db308751c9f0f6abba
Zoran <zoran@zurb.com>
2015-10-29 19:13:04 -0700

    minor changes

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ var multiparty = require('multiparty')
 
 
 var fd = new formdata();
-fd.append('attachment', fs.createReadStream('/Users/emellum/Desktop/projects/launchpad_projects/apps.zip'))
+fd.append('attachment', fs.createReadStream('/Users/zoran/Downloads/large-file.zip'))
 fd.append('name', 'test');
 fd.append('description', 'test');
 fd.append('token', 'b3b2a169f471742feb38e263c5043812fc71b654');
 fd.append('project_id', 2);
 var headers = fd.getHeaders();
-headers.Authentication = 'b3b2a169f471742feb38e263c5043812fc71b654'
+headers.Authentication = 'b3b2a169f471742feb38e263c5043812fc71b654';
 var request = http.request({
-  method: 'post',
-  host: 'localhost',
-  port: 3000,
-  path: '/prototypes',
+  method: 'POST',
+  host: 'host.notable.dev',
+  // port: 3000,
+  path: '/api/v1/prototypes',
   headers: headers
 });
 
@@ -25,9 +25,9 @@ fd.pipe(request);
 
 request.on('response', function(res) {
   console.log(res.statusCode);
-  console.log(res);
-  console.log(res.hosted_url);
+  console.log(res.prototype_id);
 });
+
 /**fd.submit('localhost:8019/upload', function(err, data){
   console.log(err);
   console.log(data.hosted_url);
